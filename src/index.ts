b1336346e0d325206496dbdf678f710211228616
Pavel Bosov <bosovpn47@yandex.ru>
2024-09-24 13:33:23 +0300

    fix configs
    add types.ts
    
    edit index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
 import axios from 'axios';
+import { Pet } from './types';
 
-console.log("Hello");
+const PETS_API_URL = 'https://petstore3.swagger.io/api/v3/pet/findByStatus?status=available';
+
+const fetchPets = async (): Promise<Pet[]> => {
+    try {
+        const response = await axios.get(PETS_API_URL);
+        const pets: Pet[] = response.data;
+        return pets;
+    } catch (error) {
+        console.error('Error while retrieving data:', error);
+        throw new Error('Failed to fetch pets.');
+    }
+}
+
+fetchPets()
+    .then(pets => {
+        console.log(pets);
+    })
+    .catch(error => {
+        console.error('Error:', error.message);
+    });
